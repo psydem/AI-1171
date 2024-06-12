2f36745dbde1401006f624f2c06b53b2c127a5e1
Psydem <122212072+psydem@users.noreply.github.com>
2024-06-12 09:20:59 +0530

    Add files via upload

---
+++ BFS_impl.py
@@ -0,0 +1,31 @@
+from collections import defaultdict, deque
+
+class Graph:
+    def __init__(self):
+        self.graph = defaultdict(list)
+
+    def add_edge(self, u, v):
+        self.graph[u].append(v)
+
+    def bfs(self, start):
+        visited = set()
+        queue = deque([start])
+
+        while queue:
+            node = queue.popleft()
+            if node not in visited:
+                print(node, end=' ')
+                visited.add(node)
+                queue.extend(self.graph[node])
+
+# Example usage
+graph = Graph()
+graph.add_edge(0, 1)
+graph.add_edge(0, 2)
+graph.add_edge(1, 2)
+graph.add_edge(2, 0)
+graph.add_edge(2, 3)
+graph.add_edge(3, 3)
+
+print("BFS traversal starting from vertex 2:")
+graph.bfs(2)
